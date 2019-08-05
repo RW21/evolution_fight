38062808a72f7235bf4618f03b2b0dcf607481d8
RW21 <togotonext@gmail.com>
2019-08-05 17:30:53 +1000

    Added test

--- test_monster.py
+++ test_monster.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 from monster import *
+from monster_image import MonsterImage
 
 monster_1 = Monster()
 monster_1.directions.monster = 100
@@ -18,3 +19,16 @@ class TestMonster(TestCase):
         assert monster_1.directions.monster == 100 \
                and monster_1.directions.food == 100 \
                and monster_1.directions.water == 100
+
+    def test_monster_image_combination(self):
+        monster_1 = Monster()
+        monster_1.set_random_monster()
+
+        monster_2 = Monster()
+        monster_2.set_random_monster()
+
+        monster_3 = Monster()
+        monster_3.genes.image = MonsterImage(
+            dict_of_parts=monster_1.genes.image.generate_combined_parts(monster_2.genes.image))
+
+        monster_3.genes.image.generate_image().save('test.jpg')
